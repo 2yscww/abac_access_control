01c8f37a7a948584cafb7f7f48a14b043ab35c88
l1mit <BrokenRing31@gmail.com>
2026-01-12 11:20:24 +0800

    init commit

---
+++ backend/abac-access-control/src/main/java/com/xie/platform/abac_access_control/AbacAccessControlApplication.java
@@ -0,0 +1,13 @@
+package com.xie.platform.abac_access_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbacAccessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbacAccessControlApplication.class, args);
+	}
+
+}
---
+++ backend/abac-access-control/src/test/java/com/xie/platform/abac_access_control/AbacAccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xie.platform.abac_access_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbacAccessControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
